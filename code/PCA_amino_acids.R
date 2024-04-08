95a403d9a6a62d71ee7ce5b6c17e7bef799e4de0
RowanDurrant <r.durrant.1@research.gla.ac.uk>
2024-04-08 11:50:11 +0100

    pca stuff

--- code/PCA_amino_acids.R
+++ code/PCA_amino_acids.R
@@ -15,8 +15,51 @@ df = df[2:nrow(df),]
 rownames = df$`Accession no.`
 df = df[,2:ncol(df)]
 
-df <- sapply(df, as.numeric )
 rownames(df) = rownames
+
+df$F_codons = NA
+df$L_codons = NA
+df$I_codons = NA
+df$V_codons = NA
+df$S_codons = NA
+df$P_codons = NA
+df$T_codons = NA
+df$A_codons = NA
+df$Y_codons = NA
+df$H_codons = NA
+df$Q_codons = NA
+df$N_codons = NA
+df$K_codons = NA
+df$D_codons = NA
+df$E_codons = NA
+df$C_codons = NA
+df$R_codons = NA
+df$G_codons = NA
+
+for(k in 1:nrow(df)){
+  df$F_codons[k] = sum(as.numeric(df[k,1:2]))
+  df$L_codons[k] = sum(as.numeric(df[k,3:8]))
+  df$I_codons[k] = sum(as.numeric(df[k,9:11]))
+  df$V_codons[k] = sum(as.numeric(df[k,12:15]))
+  df$S_codons[k] = sum(as.numeric(df[k,16:21]))
+  df$P_codons[k] = sum(as.numeric(df[k,22:25]))
+  df$T_codons[k] = sum(as.numeric(df[k,26:29]))
+  df$A_codons[k] = sum(as.numeric(df[k,30:33]))
+  df$Y_codons[k] = sum(as.numeric(df[k,34:35]))
+  df$H_codons[k] = sum(as.numeric(df[k,36:37]))
+  df$Q_codons[k] = sum(as.numeric(df[k,38:39]))
+  df$N_codons[k] = sum(as.numeric(df[k,40:41]))
+  df$K_codons[k] = sum(as.numeric(df[k,42:43]))
+  df$D_codons[k] = sum(as.numeric(df[k,44:45]))
+  df$E_codons[k] = sum(as.numeric(df[k,46:47]))
+  df$C_codons[k] = sum(as.numeric(df[k,48:49]))
+  df$R_codons[k] = sum(as.numeric(df[k,51:55]))
+  df$G_codons[k] = sum(as.numeric(df[k,56:59]))
+}
+
+
+df <- sapply(df, as.numeric )
+df = df[,60:ncol(df)]
 
 df = remove_constant(df)
 
@@ -26,7 +69,6 @@ pc <- prcomp(df,
 attributes(pc)
 
 metadata = read.csv("sequence_data/metadata.csv")
-
 
 g <- ggbiplot(pc,
               obs.scale = 1,
@@ -45,9 +87,9 @@ df2$clade = metadata$Clade
 df2$Accession = metadata$Accession
 
 df2$clade = factor(df2$clade, c("Cosmo AF1b", "Cosmo AM2a", "Arctic A", "Asian SEA2a", 
-                               "Asian SEA2b", 
-                               "Bat TB1",
-                               "Bat DR", "Bat EF-E2","RAC-SK SCSK", "Bat LC"))
+                                "Asian SEA2b", 
+                                "Bat TB1",
+                                "Bat DR", "Bat EF-E2","RAC-SK SCSK", "Bat LC"))
 
 
 my_pal <- c("#332288","#88CCEE","#CCDDAA","#44AA99","#117733",  
@@ -79,41 +121,10 @@ g1 = ggplot(data = df2, aes(x = PC1, y = PC2))+
                                 "RAC-SK SCSK\n(skunk)",
                                 "Bat LC\n(hoary bat)"),
                      values = c(17,17,17,17,17,16,16,16,17,16))+
-  xlab("PC1 (24.6% explained var.)") + 
-  ylab("PC2 (22.0% explained var.)")+
+  xlab("PC1 (33.1% explained var.)") + 
+  ylab("PC2 (15.0% explained var.)")+
   theme_bw() + ylim(-10, 10) + xlim(-10, 10)+
   coord_axes_inside(labels_inside = TRUE) +
   theme(legend.position = "bottom", legend.box = "vertical")
 
 g1
-
-png("plots/Figure 5.png", width = 7.5, height = 7.5, units = 'in', res = 600)
-g1
-dev.off()
-
-write.csv(df2, "output_data/PCA_output.csv")
-
-# Helper function 
-#::::::::::::::::::::::::::::::::::::::::
-var_coord_func <- function(loadings, comp.sdev){
-  loadings*comp.sdev
-}
-# Compute Coordinates
-#::::::::::::::::::::::::::::::::::::::::
-loadings <- pc$rotation
-sdev <- pc$sdev
-var.coord <- t(apply(loadings, 1, var_coord_func, sdev))
-var.cos2 <- var.coord^2
-comp.cos2 <- apply(var.cos2, 2, sum)
-contrib <- function(var.cos2, comp.cos2){var.cos2*100/comp.cos2}
-var.contrib <- t(apply(var.cos2,1, contrib, comp.cos2))
-
-pc2 = var.contrib[,"PC2"]
-barplot(pc2)
-max(pc2)
-names(pc2[pc2>5])
-
-pc1 = var.contrib[,"PC1"]
-barplot(pc1)
-max(pc1)
-names(pc1[pc1>4.4])
